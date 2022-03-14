80527f9aa934c38ec00e6b564ce9fa8e9f987414
Soufiane  Channani <schannani@SMB.SQLI.COM>
2022-03-14 15:23:10 +0100

    cat

---
+++ Animal.java
@@ -0,0 +1,16 @@
+class Animal{
+
+    public static String getdefault(){
+        return "hello!";
+    }
+
+    public static public static void main(String[] args) {
+        public String str=args[0];
+        public String output="";
+        if(str.equalsIgnoreCase("cat")) output="miou!";
+        else output =getdefault();
+
+        System.out.println(output);
+    }
+    
+}
