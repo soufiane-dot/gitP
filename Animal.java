4f82e83af894517b338a1e9653c237b7067c7544
Soufiane  Channani <schannani@SMB.SQLI.COM>
2022-03-14 15:27:32 +0100

    master animal

--- Animal.java
+++ Animal.java
@@ -7,8 +7,7 @@ class Animal{
     public static public static void main(String[] args) {
         public String str=args[0];
         public String output="";
-        if(str.equalsIgnoreCase("cat")) output="miou!";
-        else output =getdefault();
+       output =getdefault();
 
         System.out.println(output);
     }
