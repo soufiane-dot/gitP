4ae6575a3834ee4ec274a43041301b3f5de961ae
Soufiane  Channani <schannani@SMB.SQLI.COM>
2022-03-14 15:30:15 +0100

    bird

--- Animal.java
+++ Animal.java
@@ -3,10 +3,13 @@ class Animal {
     public static String getdefault(){
         return "hello!";
         }
+        public  static String getBirdGreeting(){
+        return "twit!";
+        }
     public static void main(String[]args){
         String str=args[0];
         String output="";
-        if(str.equalsIgnoreCase("cat")) output = str;
+        if(str.equalsIgnoreCase("BIRD")) output = getBirdGreeting();
         else output = getdefault();
 
         System.out.println(output);
